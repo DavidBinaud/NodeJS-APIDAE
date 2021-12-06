e763070f53890cb8ac44d7410206974875173d31
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 11:40:02 +0100

    TP2 - Advanced  - 8

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,7 +1,18 @@
-const tab = [1, [2, [3, [4]], 5]]
+const obj = { a: { b: 1 }, c: 2, d: {e: {f: 5}} , g:9}
 
-let f = function(tab, depth){
-	return tab.flat(depth)
+let f = function(object, properties){
+	let tab = {}
+	properties.forEach(prop =>{
+			let depth = prop.split('.')
+			let cur
+			if(depth.length>1){
+				cur = f(object[depth[0]], [depth.slice(1).join('.')])
+			} else {
+				cur = object[prop]	
+			}
+			tab[depth[0]] = cur	
+	})
+	return  tab
 }
 
-console.log(f(tab, 2))
+console.log(f(obj, ['a.b', 'c', 'd.e.f']))
