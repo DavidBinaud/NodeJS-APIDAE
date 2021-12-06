9679561c2c8963af32daf09f219e3c5a75d33459
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 11:16:11 +0100

    TP2 - Advanced  - 7

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,7 +1,7 @@
-const tab = [1, 2, 3, 4, 5]
+const tab = [1, [2, [3, [4]], 5]]
 
-let f = function(tab, numberToRemove){
-	return tab.slice(numberToRemove)
+let f = function(tab, depth){
+	return tab.flat(depth)
 }
 
 console.log(f(tab, 2))
