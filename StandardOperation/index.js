5cebd1ef30077ebfa70f8d9e05a672f46e706342
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 10:57:48 +0100

    TP2 - Advanced  - 1

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,9 +1,6 @@
-const obj = {
-  a: 1,
-  b: 2,
-  c: 3
-}
+const tab = [[4, 54, 97, 46, 3], [2, 5, 78, 64], [46, 28, 29, 72, 3, 5]]
 
-let tab = Object.values(obj)
+let maxTab = tab.map(t => t.reduce((max, cur) => cur > max ? cur : max))
 
-console.log(obj, tab)
+console.log(tab)
+console.log(maxTab)
