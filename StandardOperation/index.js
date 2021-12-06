ad21af693c950c2668a5ce85e74b776c2a982ad6
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 11:07:46 +0100

    TP2 - Advanced  - 4

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,5 +1,11 @@
-const tab1 = [1, 6, 3, 4]
-const tab2 = [6, 3, 7, 8, 9]
+const tab = [1, 2, 3, 1, 2, 3]
 
-let diff = tab1.filter(e => !tab2.includes(e)).concat(tab2.filter(e => !tab1.includes(e)))
-console.log(diff)
+// TODO function remove
+
+let remove = function(tab, ...Args){
+	Args.forEach(value => tab = tab.filter(e => e != value))
+	return tab
+}
+
+const result = remove(tab, 2, 3)
+console.log(result)
