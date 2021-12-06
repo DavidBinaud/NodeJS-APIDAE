001b8b2ef9b5db53986fa176489011dd2b2eea24
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 10:45:03 +0100

    TP2 - array - 10

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,2 +1,8 @@
-const tab = ['a', 'b', 'c', 'd']
-console.log(tab, tab.indexOf('b'))
+const students = [
+  { name: 'John', money: 10 },
+  { name: 'Todd', money: 25 },
+  { name: 'Jack', money: 33 },
+  { name: 'Walter', money: 18 }
+]
+
+console.log(students.reduce((acc,e)=>acc + parseInt(e.money),0))
