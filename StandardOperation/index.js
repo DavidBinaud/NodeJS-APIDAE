36a17b7204971fac22c8e818d6664435ccc98d88
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-06 11:00:07 +0100

    TP2 - Advanced  - 1 bis

--- StandardOperation/index.js
+++ StandardOperation/index.js
@@ -1,6 +1,9 @@
 const tab = [[4, 54, 97, 46, 3], [2, 5, 78, 64], [46, 28, 29, 72, 3, 5]]
 
 let maxTab = tab.map(t => t.reduce((max, cur) => cur > max ? cur : max))
+//OU
+let mathMaxTab = tab.map(t => Math.max(...t))
 
 console.log(tab)
 console.log(maxTab)
+console.log('with Math.max: ', mathMaxTab)
