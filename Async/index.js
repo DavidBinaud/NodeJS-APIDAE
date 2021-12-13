45741c4c945a5b05c8acfbffeff7cc303d2af42c
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-13 08:40:56 +0100

    Async - Exo 2

--- Async/index.js
+++ Async/index.js
@@ -1,5 +1,7 @@
  const fs = require('fs/promises');
 
- fs.readFile('./text.txt','utf-8').then((req)=>{
-    console.log(req);
+ fs.readFile('./text.txt','utf-8').then((req1)=>{
+    fs.readFile(req1.trim(),'utf-8').then((req2)=>{
+        console.log(req2);
+    })
  })
